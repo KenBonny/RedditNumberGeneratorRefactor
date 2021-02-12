b84d40890f3bd9b5c349814443bff4a4d1fb6752
Ken Bonny <bonny.ken+github@gmail.com>
2021-02-12 13:17:04 +0100

    make loop end dependent on the same check as the value print

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace RedditNumberGeneratorRefactor
         {
             var numberGen = new Random();
             int randomValue;
+            bool goalReached;
 
             var count = 0;
             do
@@ -16,14 +17,14 @@ namespace RedditNumberGeneratorRefactor
                 count++;
 
                 randomValue = numberGen.Next(1, 347500);
-                var goalReached = randomValue >= goal;
+                goalReached = randomValue >= goal;
                 if (goalReached)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
                 Console.WriteLine(randomValue);
             }
-            while (randomValue < goal);
+            while (!goalReached);
 
 
             Console.WriteLine("\nit took you " + count + " tries to get number " + randomValue);

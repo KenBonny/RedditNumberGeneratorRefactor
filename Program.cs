5abc312c94897bbc8f3d041c8f78900164057fc3
Ken Bonny <bonny.ken+github@gmail.com>
2021-02-12 13:15:22 +0100

    generate random value before checking the goal has been reached so the value that triggers the stop is printed

--- Program.cs
+++ Program.cs
@@ -8,21 +8,19 @@ namespace RedditNumberGeneratorRefactor
         static void Main()
         {
             var numberGen = new Random();
-            var randomValue = numberGen.Next(1, 347500);
+            int randomValue;
 
             var count = 0;
             do
             {
                 count++;
 
+                randomValue = numberGen.Next(1, 347500);
                 if (randomValue >= goal)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                 }
                 Console.WriteLine(randomValue);
-                randomValue = numberGen.Next(1, 347500);
-              
-                
             }
             while (randomValue < goal);
 
